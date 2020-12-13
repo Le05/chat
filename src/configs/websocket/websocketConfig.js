78cf09c69cdb2bde7638d6c1f71c78a9702d82c4
Leonardo Pereira <leosantospereira10@hotmail.com>
2020-12-13 20:45:52 -0300

    t

--- src/configs/websocket/websocketConfig.js
+++ src/configs/websocket/websocketConfig.js
@@ -1,15 +1,17 @@
 // const connection = require('./database/conections');
 
 exports.setupWebsocket = (io) => {
-    io.on('connection', async socket => {
-        io.emit("init", "Bem vindo ao MMmodel");
-        let { user_id } = socket.handshake.query;
-        let websocket_id = socket.id;
-        // await connection('users').where('id',user_id).update('websocketId',websocket_id);
+    //quando connectar eu vou fazer isso
+    io.on('connection',connection());
+}
 
-        socket.on('message', data => {
-            io.emit('message',data);
-        });
+function connection(io,socket){
+    io.emit("init", "Bem vindo ao MMmodel");
+    let { user_id } = socket.handshake.query;
+    let websocket_id = socket.id;
+    // await connection('users').where('id',user_id).update('websocketId',websocket_id);
+    console.log(socket.id);
+    socket.on('message', data => {
+        io.emit('message',data);
     });
-
 }
